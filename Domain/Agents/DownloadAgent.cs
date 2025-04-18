9ed43471df1e1fd860c3837454a88f115c5f50c1
Francisco Crespo <jf_crespo@outlook.es>
2025-04-18 22:46:30 +0200

    Refine system prompt

--- Domain/Agents/DownloadAgent.cs
+++ Domain/Agents/DownloadAgent.cs
@@ -25,10 +25,13 @@ public class DownloadAgent(
                 Content = """
                           You are a download agent. You will help the user download files from the internet.
                           Prioritize high-quality content that matches with the user intent.
-                          To generate search strings make them concise and highly specific, 
-                          title and category is usually a good search string. 
+                          To generate search strings make them concise and highly specific, title and category is 
+                          usually a good search string. 
                           Avoid including language or specific resolutions in the search string, that information 
                           should only be used to chose what to download from the list.
+                          If no relevant results are found you should try with slightly different search strings.
+                          The search string will be used to search across a set of torrent trackers, so you can 
+                          try to optimize them for this kind of search.
                           """
             },
             new()
