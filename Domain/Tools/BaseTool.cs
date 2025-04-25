519604d2f3eb8ff3981c9f58a928a6703f223d5a
Francisco Crespo <jf_crespo@outlook.es>
2025-04-25 16:08:21 +0200

    Refactor parameter parsing in BaseTool and add unit tests for CleanupTool; improve error handling and validation

--- Domain/Tools/BaseTool.cs
+++ Domain/Tools/BaseTool.cs
@@ -5,15 +5,23 @@ namespace Domain.Tools;
 
 public abstract class BaseTool
 {
-    protected static T ParseParams<T>(JsonNode? parameters) where T : class
+    protected T ParseParams<T>(JsonNode? parameters) where T : class
     {
-        var typedParams = parameters?.Deserialize<T>();
-        if (typedParams is null)
+        try
         {
-            throw new ArgumentNullException(
-                nameof(parameters), $"{typeof(FileDownloadTool)} cannot have null parameters");
-        }
+            var typedParams = parameters?.Deserialize<T>();
+            if (typedParams is null)
+            {
+                throw new ArgumentNullException(
+                    nameof(parameters), $"{GetType().Name} cannot have null parameters");
+            }
 
-        return typedParams;
+            return typedParams;
+        }
+        catch (JsonException ex)
+        {
+            throw new ArgumentException(
+                $"Failed to deserialize parameters for {GetType().Name}", nameof(parameters), ex);
+        }
     }
 }
