4acebd14252f3885b189a44339ca9d15286de806
Francisco Crespo <jf_crespo@outlook.es>
2025-05-03 21:06:55 +0200

    Update ChatMonitor to clarify stop reason in response message format

--- Domain/Monitor/ChatMonitor.cs
+++ Domain/Monitor/ChatMonitor.cs
@@ -38,7 +38,7 @@ public class ChatMonitor(
             {
                 var mainMessage = response.Content;
                 var toolMessage = string.Join('\n', response.ToolCalls.Select(x => x.ToString()));
-                mainMessage = $"{mainMessage}\n<code>{response.StopReason}</code>";
+                mainMessage = $"{mainMessage}\nStop reason: <code>{response.StopReason}</code>";
 
                 var trimMainMessage = mainMessage.Left(2000);
                 var trimToolMessage = toolMessage.Left(2000);
