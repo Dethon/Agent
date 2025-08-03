ac955b89b6d8e77fba33e23a713a77adc30af3fd
Francisco Crespo <jf_crespo@outlook.es>
2025-08-04 00:39:01 +0200

    Implement agent launcher to streamline service startup and command handling

---
+++ Agent/App/Launcher.cs
@@ -0,0 +1,28 @@
+﻿using Agent.Settings;
+using Microsoft.Extensions.Hosting;
+
+namespace Agent.App;
+
+public static class Launcher
+{
+    public static async Task StartAgent(this IHost host, CommandLineParams cmdParams)
+    {
+        var action = ResolveAction(cmdParams);
+        await host.StartAsync();
+        await action(host.Services);
+        await host.StopAsync();
+    }
+
+    private static Func<IServiceProvider, Task> ResolveAction(CommandLineParams cmdParams)
+    {
+        if (cmdParams.IsDaemon)
+        {
+            return Monitoring.Start;
+        } 
+        if(cmdParams.Prompt is not null)
+        {
+            return services => Command.Start(services, cmdParams.Prompt);
+        }
+        throw new ArgumentException("Invalid command line parameters. Please provide a prompt or run in daemon mode.");
+    }
+}
--- Agent/Program.cs
+++ Agent/Program.cs
@@ -6,15 +6,6 @@ var builder = Host.CreateApplicationBuilder(args);
 var settings = builder.Configuration.GetSettings();
 var cmdParams = ConfigModule.GetCommandLineParams(args);
 builder.Services.ConfigureJack(settings, cmdParams);
-using var host = builder.Build();
 
-await host.StartAsync();
-if (cmdParams.IsDaemon)
-{
-    await Monitoring.Start(host.Services);
-}
-if(cmdParams.Prompt is not null)
-{
-    await Command.Start(host.Services, cmdParams.Prompt);
-}
-await host.StopAsync();
+using var host = builder.Build();
+await host.StartAgent(cmdParams);
