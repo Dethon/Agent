10e10fc5198ffd34de8ec39a0d5ad6fd0687ebc4
Francisco Crespo <jf_crespo@outlook.es>
2026-02-04 01:00:59 +0100

    Add Source property to ChatPrompt DTO

--- Domain/DTOs/ChatPrompt.cs
+++ Domain/DTOs/ChatPrompt.cs
@@ -11,4 +11,5 @@ public record ChatPrompt
     public required int MessageId { get; init; }
     public required string Sender { get; init; }
     public string? AgentId { get; init; }
+    public MessageSource Source { get; init; } = MessageSource.WebUi;
 }
