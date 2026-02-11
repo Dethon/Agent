8d0e5bdcbd59e2b40008273bda472fa77accc56f
Francisco Crespo <jf_crespo@outlook.es>
2026-02-12 00:41:12 +0100

    test: add adversarial tests for VAPID config exposure

--- Tests/Unit/WebChat/VapidConfigTests.cs
+++ Tests/Unit/WebChat/VapidConfigTests.cs
@@ -20,4 +20,33 @@ public sealed class VapidConfigTests
 
         config.VapidPublicKey.ShouldBeNull();
     }
+
+    [Fact]
+    public void AppConfig_BackwardCompatibility_TwoArgsStillWorks()
+    {
+        var config = new AppConfig(null, []);
+
+        config.VapidPublicKey.ShouldBeNull();
+        config.AgentUrl.ShouldBeNull();
+        config.Users.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void AppConfig_EmptyStringVapidKey_IsPreservedNotNull()
+    {
+        var config = new AppConfig("http://localhost:5000", [], "");
+
+        config.VapidPublicKey.ShouldNotBeNull();
+        config.VapidPublicKey.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void AppConfig_VapidPublicKey_DoesNotExposePrivateKey()
+    {
+        var config = new AppConfig("http://localhost:5000", [], "BPublicKeyOnly");
+
+        var properties = typeof(AppConfig).GetProperties();
+        properties.ShouldNotContain(p => p.Name.Contains("Private", System.StringComparison.OrdinalIgnoreCase));
+        properties.ShouldNotContain(p => p.Name.Contains("Secret", System.StringComparison.OrdinalIgnoreCase));
+    }
 }
