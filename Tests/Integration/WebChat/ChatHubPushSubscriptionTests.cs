8714c6effc8ee67e180d98c7a7b11ffebdb9a589
Francisco Crespo <jf_crespo@outlook.es>
2026-02-12 00:46:23 +0100

    test: add adversarial tests for ChatHub push subscription

--- Tests/Integration/WebChat/ChatHubPushSubscriptionTests.cs
+++ Tests/Integration/WebChat/ChatHubPushSubscriptionTests.cs
@@ -1,4 +1,6 @@
+using Domain.Contracts;
 using Domain.DTOs.WebChat;
+using Infrastructure.Clients.Messaging.WebChat;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
 using Shouldly;
@@ -66,5 +68,53 @@ public sealed class ChatHubPushSubscriptionTests(WebChatServerFixture fixture)
             unregisteredConnection.InvokeAsync("UnsubscribePush", "https://endpoint"));
 
         await unregisteredConnection.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task SubscribePush_ThenUnsubscribe_RoundTrip()
+    {
+        var endpoint = $"https://fcm.googleapis.com/fcm/send/{Guid.NewGuid():N}";
+        var subscription = new PushSubscriptionDto(endpoint, "key123", "auth456");
+
+        await _connection.InvokeAsync("SubscribePush", subscription);
+
+        var store = fixture.GetService<IPushSubscriptionStore>();
+        var allBefore = await store.GetAllAsync();
+        allBefore.ShouldContain(x => x.UserId == "push-test-user" && x.Subscription.Endpoint == endpoint);
+
+        await _connection.InvokeAsync("UnsubscribePush", endpoint);
+
+        var allAfter = await store.GetAllAsync();
+        allAfter.ShouldNotContain(x => x.Subscription.Endpoint == endpoint);
+    }
+
+    [Fact]
+    public async Task SubscribePush_FromTwoConnections_SameUser_StoresBoth()
+    {
+        var endpoint1 = $"https://fcm.googleapis.com/fcm/send/{Guid.NewGuid():N}";
+        var endpoint2 = $"https://fcm.googleapis.com/fcm/send/{Guid.NewGuid():N}";
+
+        var connection2 = fixture.CreateHubConnection();
+        await connection2.StartAsync();
+        await connection2.InvokeAsync("RegisterUser", "push-test-user");
+
+        await _connection.InvokeAsync("SubscribePush", new PushSubscriptionDto(endpoint1, "k1", "a1"));
+        await connection2.InvokeAsync("SubscribePush", new PushSubscriptionDto(endpoint2, "k2", "a2"));
+
+        var store = fixture.GetService<IPushSubscriptionStore>();
+        var all = await store.GetAllAsync();
+        var userSubs = all.Where(x => x.UserId == "push-test-user").ToList();
+        userSubs.ShouldContain(x => x.Subscription.Endpoint == endpoint1);
+        userSubs.ShouldContain(x => x.Subscription.Endpoint == endpoint2);
+
+        await connection2.DisposeAsync();
+    }
+
+    [Fact]
+    public void NullPushNotificationService_IsNoOp()
+    {
+        var sut = new NullPushNotificationService();
+
+        Should.NotThrow(() => sut.SendToSpaceAsync("default", "title", "body", "/url").GetAwaiter().GetResult());
     }
 }
